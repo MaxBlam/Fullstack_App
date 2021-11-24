2c4617500fd5e8dc32883d51d9357722de8e12c2
MaxBlam <70974889+MaxBlam@users.noreply.github.com>
2021-11-24 09:12:57 +0100

    Added Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,55 +1,57 @@
 <template>
   <v-app>
     <v-app-bar
-      app
-      color="primary"
-      dark
+      absolute
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-btn icon class="mx-1">
+        <v-img src="@/assets/logo.svg"></v-img>
+      </v-btn>
+      <v-toolbar-title>Departures</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute bottom right temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+          class="text-right"
+        >
+          <v-list-item @click.stop="drawer = !drawer">
+            <v-icon class="ms-auto">mdi-window-close</v-icon>
+          </v-list-item>
 
+          <v-list-item>
+            <v-list-item-title>Add</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Change</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Remove</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
-      <router-view/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
   }),
 };
 </script>
